4e82c88fcff1235d8e3014c574a569503715a0a3
agik-setiawan <setiawanagik@gmail.com>
2022-01-31 16:48:20 +0700

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import i18n from '../i18n'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  if(router.locale){
+    i18n.changeLanguage(router.locale);
+  }
+  return (
+  <Component {...pageProps} />
+  )
 }
 
 export default MyApp
